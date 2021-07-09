626c4406c02345b398bfa5d873601de37dd06bc5
Nathan <nathanael.esnault@gmail.com>
2021-07-10 09:54:07 +1200

    Update Test_Xiao_NoAuto.ino

--- Design Ressources/01_Code/01_Motor_Driver/Test_Xiao_NoAuto/Test_Xiao_NoAuto.ino
+++ Design Ressources/01_Code/01_Motor_Driver/Test_Xiao_NoAuto/Test_Xiao_NoAuto.ino
@@ -1,38 +1,128 @@
-// Bounce.pde
-// -*- mode: C++ -*-
-//
-// Make a single stepper bounce from one limit to another
-//
-// Copyright (C) 2012 Mike McCauley
-// $Id: Random.pde,v 1.1 2011/01/05 01:51:01 mikem Exp mikem $
+/* ========================================
+*
+* Copyright Tonkin and Taylor Ltd, 2021
+* All Rights Reserved
+* UNPUBLISHED, LICENSED SOFTWARE.
+*
+* Metadata
+* Written by    : Nathanaël Esnault, Naser
+* Verified by   : Nathanaël Esnault
+* Creation date : 2021-05-19
+* Version       : 1.0 (finished on 2021-??-??)
+* Modifications :
+* Known bugs    :
+*
+*
+* Possible Improvements
+*
+*
+* Notes
+*
+*
+* Ressources (Boards + Libraries Manager)
+*
+* Xiao pinout
+* Xiao microcontroller:     https://wiki.seeedstudio.com/Seeeduino-XIAO/
+* AccelStepper:             http://www.airspayce.com/mikem/arduino/AccelStepper/Blocking_8pde-example.html
+*                           https://github.com/waspinator/AccelStepper
+*                         
+* Github project:           https://github.com/Saultes45/Mini-Shake-Table-UoA
+* PCB EsayEDA:               
+* Motor driver:             https://www.jss-motor.com/product_category/Stepper-motor-drivers.html
+*                           https://www.jss-motor.com/product/DM860A-2-phase-stepper-motor-driver.html
+*
+* TODO
+* Use FreeRTOS and tasks instead of linear logic
+* Find a way to transfer data from/to raspberry pi
+* Limit Switch calibration 
+*
+* ========================================
+*/
 
+// -------------------------- Includes --------------------------
+// Stepper motor driver
 #include <AccelStepper.h>
 
-// Stepper motor driver pins
+
+// -------------------------- Defines --------------------------
+
+#define CONSOLE_BAUD_RATE             115200  // Baudrate in [bauds] for serial communication to the console
+
+// Modes
+#define MODE_AUTO   1u  // Wait orders and scenarios from the Raspberry pi through USB (UART)
+#define MODE_MANUAL 0u  // Use the 2 trimpots to describe the motion
+
+// Stepper motor driver pins (logic low)
 const uint8_t PIN_MOTOR_STEP   = 2; // Digital output 3.3V
 const uint8_t PIN_MOTOR_DIR    = 3; // Digital output 3.3V
 const uint8_t PIN_MOTOR_ENA    = 4; // Digital output 3.3V, manual logic
 
+const uint16_t microSteppingFactorList[]    = {1, 200}; // Handled exclusively by the motor driver
+const uint8_t indx_microSteppingFactorList = 1; // Says to this program which physical microSteppingFactor is currently on the motor driver
+const uint8_t microSteppingFactor = 200;
+const uint8_t pulsesPerRevolution = 200 * microSteppingFactor;
+
+
 uint16_t nbr_revolutions = 1;
 
-//uint32_t scenarioPos
-
-const uint8_t stepFactor = 200;
-const uint8_t pulsesPerRevolution = 200;
+// Scenarios handling
 //const uint8_t NBR_SCENARIOS
 
+// Limit switches pins
+const uint8_t PIN_LIMIT_RIGHT   = 8; // Digital input 3.3V
+const uint8_t PIN_LIMIT_LEFT    = 9; // Digital input 3.3V
+
+// Trimpot pins
+const uint8_t PIN_TRMPT_FREQ          = A0; // Analog input 3.3V
+const uint8_t PIN_TRMPT_AMPL          = A1; // Analog input 3.3V
+
+
+
+// -------------------------- Global variables ----------------
+
+// Scenarios handling
+//uint32_t scenarioPos
+
+// Mode handling
+uint8_t crnt_Mode = MODE_MANUAL;
+uint8_t prev_Mode = MODE_MANUAL;
+
 // Define a stepper and the pins it will use
+// Precise that we use a dedicated stepper driver, thus we have 2 pins: STEP and DIR (ENA is handled manually)
 AccelStepper stepper(AccelStepper::DRIVER, PIN_MOTOR_STEP, PIN_MOTOR_DIR);
 
+// -------------------------- ISR ----------------
+
+// -------------------------- Functions declaration --------------------------
+
+
+// -------------------------- Set up --------------------------
 void setup()
 {  
 
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(CONSOLE_BAUD_RATE); //Begin serial communication (USB)
+	pinMode(LED_BUILTIN, OUTPUT);   //Set up the on-board LED (RED, close to the uUSB port)
+
+	// Indicate the start of the setup with the red on-board LED
+	//------------------------------------------------------------
+	digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED ON
+	delay(10);                         // Wait a bit
+
+
+  // Limit switches pins
+  pinMode(PIN_LIMIT_RIGHT, INPUT);
+  pinMode(PIN_LIMIT_LEFT, INPUT);
+
+
+  // Trimpot pins
+  pinMode(PIN_TRMPT_FREQ, INPUT);
+  pinMode(PIN_TRMPT_AMPL, INPUT);
+
+
   // initialize digital pin PIN_MOTOR_ENA as an output.
   pinMode(PIN_MOTOR_ENA, OUTPUT);
 
-// Disable the motor (logic inverse)
+  // Disable the motor (logic inverse)
   digitalWrite(PIN_MOTOR_ENA,HIGH);
 
   Serial.begin(115200);
@@ -40,21 +130,47 @@ void setup()
   Serial.println("Welcome to the stepper motor test");
 
   
-  Serial.println("Setting max speed to ");
-  stepper.setMaxSpeed(50000 * stepFactor);
-  stepper.setAcceleration(10000 * stepFactor);
-  stepper.moveTo(nbr_revolutions * pulsesPerRevolution * stepFactor);
-}
+  Serial.print("Setting max speed to ");
+  Serial.println();
+  stepper.setMaxSpeed(50000 * microSteppingFactor);
+  Serial.print("New max speed is ");
+  Serial.println();
 
+  Serial.print("Setting max speed to ");
+  Serial.println();
+  stepper.setAcceleration(10000 * microSteppingFactor);
+  Serial.print("New max speed is ");
+  Serial.println();
+
+  Serial.print("Setting position to reach ");
+  Serial.println();
+  stepper.moveTo(nbr_revolutions * pulsesPerRevolution * microSteppingFactor);
+
+
+} // END OF SETUP
+
+// -------------------------- Loop --------------------------
 void loop()
 {
     // If at the end of travel go to the other end
     if (stepper.distanceToGo() == 0)
     {
+      // Pause for 3s
       delay(3000);
+      // Prepare the stepper to go the other way
       stepper.moveTo(-stepper.currentPosition());
     }
       
 
     stepper.run();
 }
+
+
+
+/*--------------------------------------------------*/
+/*-------------------- Functions -------------------*/
+/*--------------------------------------------------*/
+//******************************************************************************************
+
+
+// END OF FILE
