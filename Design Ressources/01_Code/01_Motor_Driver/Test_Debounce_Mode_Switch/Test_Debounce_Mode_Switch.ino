62db1ef697cb9557fa5ca9cfd2a9d19af7fa07d6
Nathan <nathanael.esnault@gmail.com>
2021-07-16 05:48:04 +1200

    Update Test_Debounce_Mode_Switch.ino

--- Design Ressources/01_Code/01_Motor_Driver/Test_Debounce_Mode_Switch/Test_Debounce_Mode_Switch.ino
+++ Design Ressources/01_Code/01_Motor_Driver/Test_Debounce_Mode_Switch/Test_Debounce_Mode_Switch.ino
@@ -1,44 +1,44 @@
 /*
-  button_int.ino
+button_int.ino
 
 */
 
 #define BUTTON  8        // Can be any I/O pin from 0 to 10
 #define DEBOUNCE_TIME 10 // millisecond button debouncing time
 
-volatile bool flagMode = false; 
+volatile bool flagMode = false;
 volatile unsigned long last_interrupt_time = 0;
 
-void buttonISR() 
+void buttonISR()
 {
-     noInterrupts();
- unsigned long interrupt_time = millis();
- // If interrupts come faster than 200ms, assume it's a bounce and ignore
- if (interrupt_time - last_interrupt_time > 200) 
- {
-    flagMode = true; 
-    digitalWrite(LED_BUILTIN, digitalRead(BUTTON)); 
- }
- last_interrupt_time = interrupt_time;
-//}
-     //delay(DEBOUNCE_TIME);
-     interrupts();
+	noInterrupts();
+	unsigned long interrupt_time = millis();
+	// If interrupts come faster than 200ms, assume it's a bounce and ignore
+	if (interrupt_time - last_interrupt_time > 200)
+	{
+		flagMode = true;
+		digitalWrite(LED_BUILTIN, digitalRead(BUTTON));
+	}
+	last_interrupt_time = interrupt_time;
+	//}
+	//delay(DEBOUNCE_TIME);
+	interrupts();
 }
 
-void setup() { 
-     // initialize the LED pin as an output:
-     pinMode(LED_BUILTIN, OUTPUT);
-     // initialize the pushbutton pin as an input and enable the internal pull up resistor:
-     pinMode(BUTTON, INPUT);
-     attachInterrupt(digitalPinToInterrupt(BUTTON), buttonISR, CHANGE);  
+void setup() {
+	// initialize the LED pin as an output:
+	pinMode(LED_BUILTIN, OUTPUT);
+	// initialize the pushbutton pin as an input and enable the internal pull up resistor:
+	pinMode(BUTTON, INPUT);
+	attachInterrupt(digitalPinToInterrupt(BUTTON), buttonISR, CHANGE);
 }
 
 void loop() {
-    // empty right now, add code as needed.
-    if (flagMode)
-    {
-      flagMode = false;
-      Serial.print("Mode change with time: ");
-      Serial.println(millis());
-    }
+	// empty right now, add code as needed.
+	if (flagMode)
+	{
+		flagMode = false;
+		Serial.print("Mode change with time: ");
+		Serial.println(millis());
+	}
 }
