c6e7bf621d65f69c4349a41499783c7a7ee32565
Nathan <nathanael.esnault@gmail.com>
2021-07-16 05:48:07 +1200

    Update Test_AnalogReadSerial.ino

--- Design Ressources/01_Code/01_Motor_Driver/Test_AnalogReadSerial/Test_AnalogReadSerial.ino
+++ Design Ressources/01_Code/01_Motor_Driver/Test_AnalogReadSerial/Test_AnalogReadSerial.ino
@@ -1,13 +1,13 @@
 /*
-  AnalogReadSerial
+AnalogReadSerial
 
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+Reads an analog input on pin 0, prints the result to the Serial Monitor.
+Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
 
-  This example code is in the public domain.
+This example code is in the public domain.
 
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
 */
 
 
@@ -22,33 +22,33 @@ uint16_t sensorValue2 = 0;
 MedianFilter2<int> medianFilter2(200);
 
 
-void setup() 
+void setup()
 {
-  Serial.begin(115200);
+	Serial.begin(115200);
 }
 
 
-void loop() 
+void loop()
 {
-  // read the input on analog pin 0:
-  sensorValue1 = analogRead(A0);
-  median1 = medianFilter1.AddValue(sensorValue1);
+	// read the input on analog pin 0:
+	sensorValue1 = analogRead(A0);
+	median1 = medianFilter1.AddValue(sensorValue1);
 
 
-  
-  // read the input on analog pin 0:
-  sensorValue2 = analogRead(A1);
-  median2 = medianFilter2.AddValue(sensorValue2);
 
-  Serial.print(sensorValue1);
-  Serial.print(" ");
-  Serial.print(median1);
-  Serial.print(" ");
-  Serial.print(sensorValue2);
-  Serial.print(" ");
-  Serial.println(median2);
-  
-  
-  
-  delay(1);        // delay in between reads for stability
+	// read the input on analog pin 0:
+	sensorValue2 = analogRead(A1);
+	median2 = medianFilter2.AddValue(sensorValue2);
+
+	Serial.print(sensorValue1);
+	Serial.print(" ");
+	Serial.print(median1);
+	Serial.print(" ");
+	Serial.print(sensorValue2);
+	Serial.print(" ");
+	Serial.println(median2);
+
+
+
+	delay(1);        // delay in between reads for stability
 }
