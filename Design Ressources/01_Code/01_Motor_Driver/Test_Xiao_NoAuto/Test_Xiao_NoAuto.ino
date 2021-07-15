cbfacba0496d1eefc9104817e7e11306fcb31e95
Nathan <nathanael.esnault@gmail.com>
2021-07-16 05:49:31 +1200

    Update Test_Xiao_NoAuto.ino

--- Design Ressources/01_Code/01_Motor_Driver/Test_Xiao_NoAuto/Test_Xiao_NoAuto.ino
+++ Design Ressources/01_Code/01_Motor_Driver/Test_Xiao_NoAuto/Test_Xiao_NoAuto.ino
@@ -25,16 +25,16 @@
 * Xiao microcontroller:     https://wiki.seeedstudio.com/Seeeduino-XIAO/
 * AccelStepper:             http://www.airspayce.com/mikem/arduino/AccelStepper/Blocking_8pde-example.html
 *                           https://github.com/waspinator/AccelStepper
-*                         
+*
 * Github project (public):  https://github.com/Saultes45/Mini-Shake-Table-UoA
-* PCB EsayEDA (private):    https://easyeda.com/account/project/setting/basic?project=f569670f797046d5bf32fa1eb878fbe5               
+* PCB EsayEDA (private):    https://easyeda.com/account/project/setting/basic?project=f569670f797046d5bf32fa1eb878fbe5
 * Motor driver:             https://www.jss-motor.com/product_category/Stepper-motor-drivers.html
 *                           https://www.jss-motor.com/product/DM860A-2-phase-stepper-motor-driver.html
 *
 * TODO
 * Use FreeRTOS and tasks instead of linear logic
 * Find a way to transfer data from/to raspberry pi
-* Limit Switch calibration 
+* Limit Switch calibration
 *
 * ========================================
 */
@@ -120,7 +120,7 @@ AccelStepper stepper(AccelStepper::DRIVER, PIN_MOTOR_STEP, PIN_MOTOR_DIR);
 
 // -------------------------- Set up --------------------------
 void setup()
-{  
+{
 
 	Serial.begin(CONSOLE_BAUD_RATE); //Begin serial communication (USB)
 	pinMode(LED_BUILTIN, OUTPUT);   //Set up the on-board LED (RED, close to the uUSB port)
@@ -129,69 +129,69 @@ void setup()
 	//------------------------------------------------------------
 	digitalWrite(LED_BUILTIN, HIGH);   // Turn the LED ON
 
-//  // Limit switches pins
-//  pinMode(PIN_LIMIT_RIGHT, INPUT);
-//  pinMode(PIN_LIMIT_LEFT, INPUT);
-//
-//
-//  // Trimpot pins
-//  pinMode(PIN_TRMPT_FREQ, INPUT);
-//  pinMode(PIN_TRMPT_AMPL, INPUT);
+	//  // Limit switches pins
+	//  pinMode(PIN_LIMIT_RIGHT, INPUT);
+	//  pinMode(PIN_LIMIT_LEFT, INPUT);
+	//
+	//
+	//  // Trimpot pins
+	//  pinMode(PIN_TRMPT_FREQ, INPUT);
+	//  pinMode(PIN_TRMPT_AMPL, INPUT);
 
 
-  // initialize digital pin PIN_MOTOR_ENA as an output.
-  pinMode(PIN_MOTOR_ENA, OUTPUT);
+	// initialize digital pin PIN_MOTOR_ENA as an output.
+	pinMode(PIN_MOTOR_ENA, OUTPUT);
 
-  // Disable the motor (logic inverse)
-  digitalWrite(PIN_MOTOR_ENA,HIGH);
-
-
-  Serial.println("---------------------------------");
-  //Serial.print(F("\nStarting stepper motor test on ")); Serial.println(BOARD_NAME);
-  //Serial.println(SAMD_TIMER_INTERRUPT_VERSION);
-  //Serial.print(F("CPU Frequency = ")); Serial.print(F_CPU / 1000000); Serial.println(F(" MHz"));
+	// Disable the motor (logic inverse)
+	digitalWrite(PIN_MOTOR_ENA,HIGH);
 
 
-//  Serial.println("Reading frequency trimpot");
-//  // read the value from the sensor:
-//  int sensor1Value = analogRead(PIN_TRMPT_FREQ);
-//  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-//  float voltage1 = sensor1Value * (3.3 / 1023.0);
-//  // print out the value you read:
-//  //Serial.println(voltage1);
-//  Serial.println(sensor1Value);
-//
-//    Serial.println("Reading amplitude trimpot");
-//  // read the value from the sensor:
-//  int sensor2Value = analogRead(PIN_TRMPT_AMPL);
-//  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-//  float voltage2 = sensor1Value * (3.3 / 1023.0);
-//  // print out the value you read:
-//  //Serial.println(voltage1);
-//  Serial.println(sensor2Value);
+	Serial.println("---------------------------------");
+	//Serial.print(F("\nStarting stepper motor test on ")); Serial.println(BOARD_NAME);
+	//Serial.println(SAMD_TIMER_INTERRUPT_VERSION);
+	//Serial.print(F("CPU Frequency = ")); Serial.print(F_CPU / 1000000); Serial.println(F(" MHz"));
 
-  Serial.print("Current micro-steps settings: ");
-  Serial.println(microSteppingFactorList[indx_microSteppingFactorList]);
 
-  Serial.print("Current micro-steps per rotation: ");
-  Serial.println(microstepsPerRevolution);
+	//  Serial.println("Reading frequency trimpot");
+	//  // read the value from the sensor:
+	//  int sensor1Value = analogRead(PIN_TRMPT_FREQ);
+	//  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+	//  float voltage1 = sensor1Value * (3.3 / 1023.0);
+	//  // print out the value you read:
+	//  //Serial.println(voltage1);
+	//  Serial.println(sensor1Value);
+	//
+	//    Serial.println("Reading amplitude trimpot");
+	//  // read the value from the sensor:
+	//  int sensor2Value = analogRead(PIN_TRMPT_AMPL);
+	//  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
+	//  float voltage2 = sensor1Value * (3.3 / 1023.0);
+	//  // print out the value you read:
+	//  //Serial.println(voltage1);
+	//  Serial.println(sensor2Value);
 
-  
-  Serial.print("Setting max speed to ");
-  Serial.println();
-  stepper.setMaxSpeed(50000 * microSteppingFactorList[indx_microSteppingFactorList]);
-  Serial.print("New max speed is ");
-  Serial.println();
+	Serial.print("Current micro-steps settings: ");
+	Serial.println(microSteppingFactorList[indx_microSteppingFactorList]);
 
-  Serial.print("Setting max speed to ");
-  Serial.println();
-  stepper.setAcceleration(10000 * microSteppingFactorList[indx_microSteppingFactorList]);
-  Serial.print("New max speed is ");
-  Serial.println();
+	Serial.print("Current micro-steps per rotation: ");
+	Serial.println(microstepsPerRevolution);
 
-  Serial.print("Setting position to reach ");
-  Serial.println();
-  stepper.moveTo(nbr_desiredRevolutions *  microSteppingFactorList[indx_microSteppingFactorList]);
+
+	Serial.print("Setting max speed to ");
+	Serial.println();
+	stepper.setMaxSpeed(50000 * microSteppingFactorList[indx_microSteppingFactorList]);
+	Serial.print("New max speed is ");
+	Serial.println();
+
+	Serial.print("Setting max speed to ");
+	Serial.println();
+	stepper.setAcceleration(10000 * microSteppingFactorList[indx_microSteppingFactorList]);
+	Serial.print("New max speed is ");
+	Serial.println();
+
+	Serial.print("Setting position to reach ");
+	Serial.println();
+	stepper.moveTo(nbr_desiredRevolutions *  microSteppingFactorList[indx_microSteppingFactorList]);
 
 
 } // END OF SETUP
@@ -199,17 +199,17 @@ void setup()
 // -------------------------- Loop --------------------------
 void loop()
 {
-    // If at the end of travel go to the other end
-    if (stepper.distanceToGo() == 0)
-    {
-      // Pause for 3s
-      delay(3000);
-      // Prepare the stepper to go the other way
-      stepper.moveTo(-stepper.currentPosition());
-    }
-      
+	// If at the end of travel go to the other end
+	if (stepper.distanceToGo() == 0)
+	{
+		// Pause for 3s
+		delay(3000);
+		// Prepare the stepper to go the other way
+		stepper.moveTo(-stepper.currentPosition());
+	}
+	
 
-    stepper.run();
+	stepper.run();
 }
 
 
