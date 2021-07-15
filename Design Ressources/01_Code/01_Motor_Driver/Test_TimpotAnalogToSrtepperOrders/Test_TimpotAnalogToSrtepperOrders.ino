91a99a92d6c2bdad059798c827b6e7c70763f96a
Nathan <nathanael.esnault@gmail.com>
2021-07-16 05:48:02 +1200

    Update Test_TimpotAnalogToSrtepperOrders.ino

--- Design Ressources/01_Code/01_Motor_Driver/Test_TimpotAnalogToSrtepperOrders/Test_TimpotAnalogToSrtepperOrders.ino
+++ Design Ressources/01_Code/01_Motor_Driver/Test_TimpotAnalogToSrtepperOrders/Test_TimpotAnalogToSrtepperOrders.ino
@@ -1,13 +1,13 @@
 /*
-  AnalogReadSerial
+AnalogReadSerial
 
-  Reads an analog input on pin 0, prints the result to the Serial Monitor.
-  Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
-  Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
+Reads an analog input on pin 0, prints the result to the Serial Monitor.
+Graphical representation is available using Serial Plotter (Tools > Serial Plotter menu).
+Attach the center pin of a potentiometer to pin A0, and the outside pins to +5V and ground.
 
-  This example code is in the public domain.
+This example code is in the public domain.
 
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
+https://www.arduino.cc/en/Tutorial/BuiltInExamples/AnalogReadSerial
 */
 
 
@@ -62,71 +62,71 @@ float trimpot_amplitude_max = 0.5; // in [stepper shaft rotations]
 float trimpot_amplitude_min = 0.0; // in [stepper shaft rotations]
 // to use with : stepper.moveTo
 
-void setup() 
+void setup()
 {
-  Serial.begin(CONSOLE_BAUD_RATE);
+	Serial.begin(CONSOLE_BAUD_RATE);
 }
 
 
-void loop() 
+void loop()
 {
-  // read the input on analog pin 0:
-  sensorValue1 = analogRead(PIN_TRMPT_FREQ);
-  // Apply deadband
-  if (sensorValue1 <= TRMPT_DEADBAND_FREQ)
-  {
-    // Then we are in the deadband and the outpout value should be 0[LSB]
-    sensorValue1 = 0;
-    }
-  median1 = medianFilter1.AddValue(sensorValue1);
-  current_trimpotFrequency_filtered = (median1-trimpot_frequency_range_min_lsb) * (trimpot_frequency_max - trimpot_frequency_min)/(trimpot_frequency_range_max_lsb-trimpot_frequency_range_min_lsb) + trimpot_frequency_min;
-if (current_trimpotFrequency_filtered < trimpot_frequency_min)
-{
-  current_trimpotFrequency_filtered = trimpot_frequency_min;
-}
-else if(current_trimpotFrequency_filtered > trimpot_frequency_max)
-{
-  current_trimpotFrequency_filtered = trimpot_frequency_max;
-}
-
-  
-  // read the input on analog pin 1:
-  sensorValue2 = analogRead(PIN_TRMPT_AMPL);
-    // Apply deadband
-  if (sensorValue2 <= TRMPT_DEADBAND_AMPL)
-  {
-    // Then we are in the deadband and the outpout value should be 0[LSB]
-    sensorValue2 = 0;
-    }
-  median2 = medianFilter2.AddValue(sensorValue2);
-  current_trimpotAmplitude_filtered = (median2-trimpot_amplitude_range_min_lsb) * (trimpot_amplitude_max - trimpot_amplitude_min)/(trimpot_amplitude_range_max_lsb-trimpot_amplitude_range_min_lsb) + trimpot_amplitude_min;
-  if (current_trimpotAmplitude_filtered < trimpot_amplitude_min)
-  {
-    current_trimpotAmplitude_filtered = trimpot_amplitude_min;
-  }
-  else if(current_trimpotAmplitude_filtered > trimpot_amplitude_max)
-  {
-    current_trimpotAmplitude_filtered = trimpot_amplitude_max;
-  }
+	// read the input on analog pin 0:
+	sensorValue1 = analogRead(PIN_TRMPT_FREQ);
+	// Apply deadband
+	if (sensorValue1 <= TRMPT_DEADBAND_FREQ)
+	{
+		// Then we are in the deadband and the outpout value should be 0[LSB]
+		sensorValue1 = 0;
+	}
+	median1 = medianFilter1.AddValue(sensorValue1);
+	current_trimpotFrequency_filtered = (median1-trimpot_frequency_range_min_lsb) * (trimpot_frequency_max - trimpot_frequency_min)/(trimpot_frequency_range_max_lsb-trimpot_frequency_range_min_lsb) + trimpot_frequency_min;
+	if (current_trimpotFrequency_filtered < trimpot_frequency_min)
+	{
+		current_trimpotFrequency_filtered = trimpot_frequency_min;
+	}
+	else if(current_trimpotFrequency_filtered > trimpot_frequency_max)
+	{
+		current_trimpotFrequency_filtered = trimpot_frequency_max;
+	}
 
 
+	// read the input on analog pin 1:
+	sensorValue2 = analogRead(PIN_TRMPT_AMPL);
+	// Apply deadband
+	if (sensorValue2 <= TRMPT_DEADBAND_AMPL)
+	{
+		// Then we are in the deadband and the outpout value should be 0[LSB]
+		sensorValue2 = 0;
+	}
+	median2 = medianFilter2.AddValue(sensorValue2);
+	current_trimpotAmplitude_filtered = (median2-trimpot_amplitude_range_min_lsb) * (trimpot_amplitude_max - trimpot_amplitude_min)/(trimpot_amplitude_range_max_lsb-trimpot_amplitude_range_min_lsb) + trimpot_amplitude_min;
+	if (current_trimpotAmplitude_filtered < trimpot_amplitude_min)
+	{
+		current_trimpotAmplitude_filtered = trimpot_amplitude_min;
+	}
+	else if(current_trimpotAmplitude_filtered > trimpot_amplitude_max)
+	{
+		current_trimpotAmplitude_filtered = trimpot_amplitude_max;
+	}
 
-// Raw values
-//  Serial.print(sensorValue1);
-//  Serial.print(" ");
-//  Serial.print(median1);
-//  Serial.print(" ");
-//  Serial.print(sensorValue2);
-//  Serial.print(" ");
-//  Serial.println(median2);
 
-// Prints
 
-Serial.print(current_trimpotFrequency_filtered);
-Serial.print(" ");
-Serial.println(current_trimpotAmplitude_filtered);
-  
-  
-  
-  delay(1);        // delay in between reads for stability
+	// Raw values
+	//  Serial.print(sensorValue1);
+	//  Serial.print(" ");
+	//  Serial.print(median1);
+	//  Serial.print(" ");
+	//  Serial.print(sensorValue2);
+	//  Serial.print(" ");
+	//  Serial.println(median2);
+
+	// Prints
+
+	Serial.print(current_trimpotFrequency_filtered);
+	Serial.print(" ");
+	Serial.println(current_trimpotAmplitude_filtered);
+
+
+
+	delay(1);        // delay in between reads for stability
 }
