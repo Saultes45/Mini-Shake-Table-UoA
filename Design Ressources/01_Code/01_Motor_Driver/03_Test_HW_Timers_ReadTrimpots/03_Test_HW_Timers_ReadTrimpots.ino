029c437b69626e776440a75ea5b95fed6aea3185
Nathan <nathanael.esnault@gmail.com>
2021-07-16 05:45:32 +1200

    Update 03_Test_HW_Timers_ReadTrimpots.ino

--- Design Ressources/01_Code/01_Motor_Driver/03_Test_HW_Timers_ReadTrimpots/03_Test_HW_Timers_ReadTrimpots.ino
+++ Design Ressources/01_Code/01_Motor_Driver/03_Test_HW_Timers_ReadTrimpots/03_Test_HW_Timers_ReadTrimpots.ino
@@ -32,9 +32,9 @@
 
 
 #ifdef USE_TC3
-  #include <TimerTC3.h>
+#include <TimerTC3.h>
 #else
-  #include <TimerTCC0.h>
+#include <TimerTCC0.h>
 #endif
 
 
@@ -52,63 +52,63 @@ bool isLEDOn = false;
 
 
 void timerIsr()
-{    
+{
 
-/* NOTICE: 
- *  Do NOT put noInterupts(); here because this is a low priority and long execution time "task"
- *  Limit switches IRS should be able to trigger in there
+	/* NOTICE:
+*  Do NOT put noInterupts(); here because this is a low priority and long execution time "task"
+*  Limit switches IRS should be able to trigger in there
 */
 
-  // Local variable declaration
-  //----------------------------
-  uint16_t sensorValue1 = 0;
-  uint16_t sensorValue2 = 0;
+	// Local variable declaration
+	//----------------------------
+	uint16_t sensorValue1 = 0;
+	uint16_t sensorValue2 = 0;
 
-  
-    // Visual indication
-    //-------------------
-    digitalWrite(13, isLEDOn);
-    isLEDOn = !isLEDOn;
 
-    // Trimpot read and process
-    //-------------------------
-    
-    // read the input on analog pin 0:
-    sensorValue1 = analogRead(PIN_TRMPT_FREQ);
-  
-  
-    // read the input on analog pin 1:
-    sensorValue2 = analogRead(PIN_TRMPT_AMPL);
-  
+	// Visual indication
+	//-------------------
+	digitalWrite(13, isLEDOn);
+	isLEDOn = !isLEDOn;
 
-    // Display (for  <DEBUG> only)
-    //----------------------------
+	// Trimpot read and process
+	//-------------------------
+	
+	// read the input on analog pin 0:
+	sensorValue1 = analogRead(PIN_TRMPT_FREQ);
 
-    Serial.print(sensorValue1);
-    Serial.print(" ");
-    Serial.println(sensorValue2);
-     
+
+	// read the input on analog pin 1:
+	sensorValue2 = analogRead(PIN_TRMPT_AMPL);
+
+
+	// Display (for  <DEBUG> only)
+	//----------------------------
+
+	Serial.print(sensorValue1);
+	Serial.print(" ");
+	Serial.println(sensorValue2);
+	
 }
 
 
-void setup() 
+void setup()
 {
 
-    Serial.begin(CONSOLE_BAUD_RATE);
-    pinMode(13, OUTPUT);    
+	Serial.begin(CONSOLE_BAUD_RATE);
+	pinMode(13, OUTPUT);
 
 #ifdef USE_TC3
-      TimerTc3.initialize(10000);
-    TimerTc3.attachInterrupt(timerIsr);
+	TimerTc3.initialize(10000);
+	TimerTc3.attachInterrupt(timerIsr);
 #else
-    TimerTcc0.initialize(10000); // 1e6 = 1s
-    TimerTcc0.attachInterrupt(timerIsr);
+	TimerTcc0.initialize(10000); // 1e6 = 1s
+	TimerTcc0.attachInterrupt(timerIsr);
 #endif
 
 
 }
 
 void loop() {
-  
+
 
 }
