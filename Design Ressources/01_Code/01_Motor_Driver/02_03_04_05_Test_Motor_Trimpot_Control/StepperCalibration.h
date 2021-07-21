b2c6ba5641790c061446d4d9f558ddc4caadb66b
Nathan <nathanael.esnault@gmail.com>
2021-07-22 09:12:05 +1200

    Create StepperCalibration.h

---
+++ Design Ressources/01_Code/01_Motor_Driver/02_03_04_05_Test_Motor_Trimpot_Control/StepperCalibration.h
@@ -0,0 +1,49 @@
+/* ========================================
+*
+* Copyright University of Auckland Ltd, 2021
+* All Rights Reserved
+* UNPUBLISHED, LICENSED SOFTWARE.
+*
+* Metadata
+* Written by    : Nathanaël Esnault
+* Verified by   : Nathanaël Esnault
+* Creation date : 2021-07-22
+* Version       : 0.1 (finished on 2021-..-..)
+* Modifications :
+* Known bugs    :
+*
+*
+* Possible Improvements
+* Remove deadband from the ADC range in the conversion
+* Check for #ifdef SERIAL_VERBOSE where there is a "Serial.print..."
+*
+* Notes
+*
+*
+* Ressources (Boards + Libraries Manager)
+*
+*
+* TODO
+*
+* ========================================
+*/
+
+
+// -------------------------- Global variables ----------------
+// Stepper calibration
+//---------------------
+float ustepsPerMM_calib       = 0.0;        // This parameter holds the calibration parameter
+float distanceBetweenLS_MM    = 500.0;     // For stepper calibration
+long distanceBetweenLS_uSteps = 0;          // For stepper calibration
+//bool abortCalibration         = true;       // A variable that tells if we need to stop the calibration
+bool calibrationSuccess       = false;      // A variable that tells if the calibration was sucessful
+
+bool needCalibration          = true;       // Indicates if there is a current good distance calibration done
+bool abortMovement            = true;       // A (unified) variable that tells if we need to stop the centering/calibration/scenario
+bool executingCalib           = false;      // Indicates if we are currently in distance calibration with the Limit Switches
+
+
+
+
+
+// END OF THE FILE
